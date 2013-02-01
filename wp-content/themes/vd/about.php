4ad6cf178a022fa43855e96c4c0c79ccbdab9fd4
Vincent DURAND <wyllen44@gmail.com>
2013-02-01 19:47:35 +0100

    3

--- wp-content/themes/vd/about.php
+++ wp-content/themes/vd/about.php
@@ -3,7 +3,12 @@
 	<article class="front">
 		<div class="pres">
 		<img src="<?php bloginfo('template_url'); ?>/images/profil.png" class="profilimg">
-		<p>Sprite sheet animation with steps(). There is an over a year old jsFiddle but I never got around to post it.. until today! ;-) If you don’t wanna use gifs.</p>
+	<?php	$contact = new WP_Query('page_id=12'); 
+            if($contact->have_posts()) : $contact-> the_post();
+            the_content();
+
+            endif;
+            ?>
 		</div>
 		<label for="checkskills" class="boxbutton skillsbutton"><div class="tools"></div></label><input id="checkskills" type="checkbox">
 		<div class="skills">
--- wp-content/themes/vd/contact.php
+++ wp-content/themes/vd/contact.php
@@ -1,10 +1,17 @@
 <section id="contact">
 <div class="sectionheader">	<h1><?php _e( 'Contact', 'vd' ); ?></h1><div class="mail2"></div>	</div>
-	<article>
-		<p>Sprite sheet animation with steps(). There is an over a year old jsFiddle but I never got around to post it.. until today! ;-) If you don’t wanna use gifs on your site and rather PNGs for better colors.</p>
+	<article><?php
+
+			$contact = new WP_Query('page_id=16'); 
+            if($contact->have_posts()) : $contact-> the_post();
+            the_content();
+
+            endif;
+            ?>
+		
 		<label for="checkcontact" class="boxbutton contactbutton"><div class="mail"></div></label><input id="checkcontact" type="checkbox">
 		<div class="contactform">
-			contactezmoi
+			<?php echo do_shortcode('[contact-form-7 id="57" title="Contact"]'); ?>
 		</div>
 	</article>
 </section>
